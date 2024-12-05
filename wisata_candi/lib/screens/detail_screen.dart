081caf71286f8b9ffd8cdc90f9660d4d4857faa7
SteffanieAngelica <165135182+SteffanieAngelica@users.noreply.github.com>
2024-12-05 13:19:08 +0700

    final 2 (5 des 24)

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -68,18 +68,21 @@ void _loadFavoriteStatus() async {
             children: [
               //image header
               Stack(
-          
+                
                 children: [ 
-                  // padding utama
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset(
-                        widget.candi.imageAsset, 
-                        width: double.infinity, 
-                        height: 300, 
-                        fit: BoxFit.cover,
+                  // image utama
+                  Hero(
+                    tag: widget.candi.imageAsset,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          widget.candi.imageAsset, 
+                          width: double.infinity, 
+                          height: 300, 
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
