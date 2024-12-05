932f4fc052532344fa36a83d07e5749eb10a6bfb
SteffanieAngelica <165135182+SteffanieAngelica@users.noreply.github.com>
2024-12-05 13:12:32 +0700

    final 5 desember 2024

--- wisata_candi/lib/screens/sign_in_screen.dart
+++ wisata_candi/lib/screens/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -20,33 +21,64 @@ class _SignInScreenState extends State<SignInScreen> {
   bool _isSignIn = false;
   bool _obscurePassword = true;
 
-  void _signIn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String savedUsername = prefs.getString('username') ?? '';
-    final String savedpassword = prefs.getString('password') ?? '';
-    final String enteredUsername = _usernameController.text.trim();
-    final String enteredPassword = _passwordController.text.trim();
-    
-    if (enteredUsername == savedUsername && enteredPassword == savedpassword) {
-      setState(() {
-        _errorText = '';
-        _isSignIn = true;
-        prefs.setBool('isSignedIn', true);
-      });
-      // Pemanggilan untuk menghapus semua halaman dalam tumpukkan navigasi 
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.of(context).popUntil((route) => route.isFirst);
-      });
-      //Sign In berhasil, navigasikan ke layar utama 
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.pushReplacementNamed(context, '/');
-      });
-    } else {
-      setState(() {
-        _errorText = 'Nama Pengguna atau Kata Sandi Salah!';
-      });
-    }
+  Future<Map<String, String>> _retrieveAndDecryptDataFromPrefs(
+    Future<SharedPreferences> prefs,
+    ) async {
+  final sharedPreferences= await prefs;
+  final encryptedUsername = sharedPreferences.getString('username') ?? '';
+  final encryptedPassword = sharedPreferences.getString('password') ?? '';
+  final keyString = sharedPreferences.getString('key') ?? '';
+  final ivString = sharedPreferences.getString('iv') ?? '';
+
+  final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+  final iv = encrypt.IV.fromBase64(ivString);
+
+  final encrypter = encrypt.Encrypter(encrypt.AES(key));
+  final decryptedUsername = encrypter.decrypt64(encryptedUsername, iv: iv);
+  final decryptedPassword = encrypter.decrypt64(encryptedPassword, iv: iv);
+
+  return {'username': decryptedUsername, 'password': decryptedPassword};  
   }
+ 
+ void _signIn() async {
+  try {
+    final Future<SharedPreferences> prefsFuture = SharedPreferences.getInstance();
+    final String username = _usernameController.text;
+    final String password = _passwordController.text;
+    print('Sign in attempt');
+     if (username.isNotEmpty && password.isNotEmpty) {
+      final SharedPreferences prefs = await prefsFuture;
+      final data = await _retrieveAndDecryptDataFromPrefs(prefs as Future<SharedPreferences>);
+      if (data.isNotEmpty) {
+        final decryptedUsername = data['username'];
+        final decryptedPassword = data['password'];
+        if (username == decryptedUsername && password == decryptedPassword) {
+          _errorText = '';
+          _isSignIn = true;
+          prefs.setBool('isSignedIn', true);
+          // Pemanggilan untuk menghapus semua halaman dalam tumpukan navigasi
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+          Navigator.of(context).popUntil((route) => route.isFirst);
+          });
+          // Sign in berhasil, navigasikan ke layar utama
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+          Navigator.pushReplacementNamed(context, '/');
+          });
+          print('Sign in succeeded');
+        } else {
+          print('Username or password is incorrect');
+        }
+      } else {
+        print('No stored credentials found');
+      }
+    } else {
+      print('Username and password cannot be empty');
+      // Tambahkan pesan untuk kasus ketika username atau password kosong
+    }
+  } catch (e) {
+    print('An error occurred: $e');
+  }
+}
 
   @override
   Widget build(BuildContext context) {
--- wisata_candi/lib/screens/sign_up_screen.dart
+++ wisata_candi/lib/screens/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class SignUpScreen extends StatefulWidget {
   SignUpScreen({super.key});
@@ -35,11 +36,25 @@ class _SignUpScreenState extends State<SignUpScreen> {
         });
         return;
       }
-      //simpan data pengguna di SharedPreferences
-      prefs.setString('fullname', name);
-      prefs.setString('username', username);
-      prefs.setString('password', password);
 
+      if(name.isNotEmpty && username.isNotEmpty && password.isNotEmpty){
+        final encrypt.Key key = encrypt.Key.fromLength(32); 
+        final iv = encrypt.IV.fromLength(16);
+
+        final encrypter = encrypt.Encrypter(encrypt.AES(key));
+        final encryptedName = encrypter.encrypt(name, iv: iv);
+        final encryptedUsername = encrypter.encrypt(username, iv: iv);
+        final encryptedPassword = encrypter.encrypt(password, iv: iv);
+
+        //simpan data pengguna di SharedPreferences
+        prefs.setString('fullname', encryptedName.base64);
+        prefs.setString('username', encryptedUsername.base64);
+        prefs.setString('password', encryptedPassword.base64);
+        prefs.setString('key', key.base64);
+        prefs.setString('iv', iv.base64);
+      }
+
+      
       //buat navigasi ke SignInScreen
       Navigator.pushReplacementNamed(context, '/signin');
     }
